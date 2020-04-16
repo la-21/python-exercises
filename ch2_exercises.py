8a9969eb0610e03162d13b2c69ff740c21bd824b
LA <56452461+lzl21@users.noreply.github.com>
2020-04-16 16:33:19 +0800

    Update ch2_exercises.py

--- ch2_exercises.py
+++ ch2_exercises.py
@@ -1,18 +1,18 @@
 # Chapter 2: Variables, expressions, and statements
 
-# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+## Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
 
 name = input('Enter your name: ')
 print('Hello ' + name)
 
-# Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+## Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
 
 hours = int(input('Enter Hours: '))
 rate = float(input('Enter Rate: '))
 pay = hours * rate
 print('Pay: ' + str(pay))
 
-# Exercise 4: Assume that we execute the following assignment statements:
+## Exercise 4: Assume that we execute the following assignment statements:
 
 width = 17
 height = 12.0
@@ -29,7 +29,7 @@ print(str(n3) + ' : ' + str(type(n3)))
 n4 = 1 + 2 * 5
 print(str(n4) + ' : ' + str(type(n4)))
 
-# Write a program which prompts the user for a Celsius tem- perature, convert the temperature to Fahrenheit, and print out the converted temperature.
+## Exercise 5: Write a program which prompts the user for a Celsius tem- perature, convert the temperature to Fahrenheit, and print out the converted temperature.
 
 ## Celsius to Fahrenheit
 celsius = float(input('Enter temperature (Celsius): '))
