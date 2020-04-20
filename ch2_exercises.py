684a2736dee976480ce372e827a5106e7f8f9959
LA <56452461+lzl21@users.noreply.github.com>
2020-04-20 21:48:52 +0800

    Update ch2_exercises.py

--- ch2_exercises.py
+++ ch2_exercises.py
@@ -1,4 +1,12 @@
-# Chapter 2: Variables, expressions, and statements
+# Chapter 2: Variables, expressions and statements
+
+## Exercise 1: Exercise 1: Type the following statements in the Python interpreter to see what they do:
+
+5
+x = 5
+x + 1
+
+# o: 6
 
 ## Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
 
@@ -29,14 +37,14 @@ print(str(n3) + ' : ' + str(type(n3)))
 n4 = 1 + 2 * 5
 print(str(n4) + ' : ' + str(type(n4)))
 
-## Exercise 5: Write a program which prompts the user for a Celsius tem- perature, convert the temperature to Fahrenheit, and print out the converted temperature.
+## Write a program which prompts the user for a Celsius tem- perature, convert the temperature to Fahrenheit, and print out the converted temperature.
 
 ## Celsius to Fahrenheit
 celsius = float(input('Enter temperature (Celsius): '))
 fahrenheit = (celsius * 9/5) + 32
-print('Fahrenheit: ' + str(fahrenheit))
+print('Fahrenheit: ',fahrenheit)
 
 ## Fahrenheit to Celsius
 fahrenheit = float(input('Enter temperature (Fahrenheit): '))
 celsius = ((fahrenheit - 32) * 5) / 9
-print('Celsius: ' + str(celsius))
+print('Celsius: ',celsius)
