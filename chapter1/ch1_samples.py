a4a37ad7ba7564f72088175aa7223bafc2614925
Liezl Asis <liezl.asis@sentia.com>
2021-05-24 15:44:29 +0200

    create chapter folders

---
+++ chapter1/ch1_samples.py
@@ -0,0 +1,24 @@
+x=1
+x=x+1
+print(x)
+
+#Sequential Steps
+x=2
+print(x)
+x=x+2
+print(x)
+
+#Conditional Steps
+x = 5
+if x < 10:
+    print('Smaller')
+if x > 20:
+    print('Bigger')
+print('Finis')
+
+#Repeated Steps (loops)
+n = 5
+while n > 0 :
+    print(n)
+    n = n - 1
+print('Blastoff!')
